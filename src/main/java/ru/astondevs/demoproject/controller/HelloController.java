9f1c074c3faedfb7fc7603968c41088cd63b1b14
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-17 18:31:35 +0300

    release 1.0.2

--- src/main/java/ru/astondevs/demoproject/controller/HelloController.java
+++ src/main/java/ru/astondevs/demoproject/controller/HelloController.java
@@ -1,18 +1,32 @@
 package ru.astondevs.demoproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.astondevs.demoproject.service.DateService;
+import ru.astondevs.demoproject.service.RandomService;
 
 @RestController
 public class HelloController {
+    @Autowired
+    DateService dateService;
+
+    @Autowired
+    RandomService randomService;
+
     @GetMapping("/yo")
     public String hello(@RequestParam(required = false) String name) {
-        return "Hello " + (name == null ? "" : name);
+        return "Helloooooo " + (name == null ? "" : name);
     }
 
     @GetMapping("/hello")
     public String hi() {
-        return "Hi";
+        return "Hi, right now is " + dateService.getDate() + ".";
+    }
+
+    @GetMapping("/future")
+    public String future() {
+        return "You will die in " + randomService.getRandom() + " seconds";
     }
 }
