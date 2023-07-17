608085245439747b7595f17d0879e43f72ee4b1c
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-17 17:22:56 +0300

    release 1.0.1

---
+++ src/main/java/ru/astondevs/demoproject/controller/HelloController.java
@@ -0,0 +1,18 @@
+package ru.astondevs.demoproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/yo")
+    public String hello(@RequestParam(required = false) String name) {
+        return "Hello " + (name == null ? "" : name);
+    }
+
+    @GetMapping("/hello")
+    public String hi() {
+        return "Hi";
+    }
+}
